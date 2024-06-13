27f58add201ba7f2e2792d43d762121dc71e53e8
unknown <innocentedenene@gmail.com>
2024-06-13 10:29:07 +0200

    creation migration carriere

---
+++ app/Database/Migrations/2024-06-13-072519_CreateOffreTable.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateOffreTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'offre_reference' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'description' => [
+                'type' => 'TEXT',
+                'constraint' => '255',
+            ],
+            'offre_localisation' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'offre_type' => [
+                'type' => 'INT',
+                'constraint' => '255',
+            ],
+            'offre_duration' => [
+                'type' => 'TEXT',
+                'constraint' => '255',
+            ],
+            'offre_nb_poste' => [
+                'type' => 'TEXT',
+                'constraint' => '255',
+            ],
+            'offre_status' => [
+                'type' => 'INT',
+                'DEFAULT' => '0',
+            ],
+            'offre_date_end' => [
+                'type' => 'DATE',
+                'constraint' => '255',
+            ],
+            'offre_file' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'created_at' => [
+                'type' => 'timestamp',
+                'constraint' => '255',
+            ],
+            'updated_at' => [
+                'type' => 'timestamp',
+                'constraint' => '255',
+            ],
+            'user_id' => [
+                'type' => 'INT',
+                'constraint' => '255',
+            ],
+            'created_at timestamp default current_timestamp',
+            'updated_at timestamp default current_timestamp on update current_timestamp',
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('offre');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('offre');
+    }
+}
---
+++ app/Models/OffreModel.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class OffreModel extends Model
+{
+    protected $table            = 'offre';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = ['offre_reference, offre_description, ofrre_localisation, offre_type, offre_duration, offre_nb_poste, offre_status, offre_date_end, offre_file, created_at, updated_at, user_id' ];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
